283b19dea42eb6dc8da05c6b1ec88f75deae27cc
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-07 17:55:36 +0700

    add all file rn

---
+++ src/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Footer.css';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div className="footer__left">
+				<img className="footer__song__icon" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="" />
+				<br />
+				<p>Nama Lagu</p>
+				<p>Nama Penyanyi dan album</p>
+			</div>
+
+			<div className="footer__center">
+				<ShuffleIcon className="footer__green" />
+				<SkipPreviousIcon className="footer__icon" />
+				<PlayCircleFilledIcon fontSize="large" className="footer__icon" />
+				<SkipNextIcon className="footer__icon" />
+				<RepeatIcon className="footer__green" />
+			</div>
+
+			<div className="footer__right">
+				<Grid container spacing={2}>
+					<Grid item>
+						<PlaylistPlayIcon />
+					</Grid>
+					<Grid item>
+						<VolumeDownIcon />
+					</Grid>
+					<Grid item xs>
+						<Slider />
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+export const initialState = {
+	user: null,
+	playlists: [],
+	playing: false,
+	item: null,
+	token:
+		'BQBrehtFSyV2MGBDPMiEumniAsOYWJUtQ1GwmZKJCGR7-Rwo-RBXo7OPI8X6VK2lLzd5kcKrLwwD_cXzXy5m1OClJgqst101ExABKdV3okbLtDupBsTSl9lsmc4f6mQlRsAagN95k8qJlW4LJvk2J9IDgrvHyoQ'
+};
+
+const reducer = (state, action) => {
+	console.log(action);
+
+	switch (action.type) {
+		case 'SET_USER':
+			return {
+				...state,
+				user: action.user
+			};
+
+		case 'SET_TOKEN':
+			return {
+				...state,
+				token: action.token
+			};
+
+		case 'SET_PLAYLISTS':
+			return {
+				...state,
+				playlists: action.playlists
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default reducer;
