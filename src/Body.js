4cd60eb55fc6d29335e78a84bfb2161af539fd40
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-08 07:20:33 +0700

    adding header and body

--- src/Body.js
+++ src/Body.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './Body.css';
 import Header from './Header';
 
-function Body() {
-    return (
-        <div className="body">
-            <Header />
+function Body({ spotify }) {
+	return (
+		<div className="body">
+			<Header spotify={spotify} />
+
+			<div className="body__info">
+				<img
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRVM08tUfWsNNG7Rh8AmDs5WIJ39ElUgMGmA&usqp=CAU"
+					alt=""
+				/>
+
+				<div className="body__infoText" >
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description</p>
         </div>
-    )
+			</div>
+		</div>
+	);
 }
 
-export default Body
+export default Body;
